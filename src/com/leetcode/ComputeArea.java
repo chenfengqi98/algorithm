d40d888f6c41bd98d2a2feacf8c98f563365edd1
chenqi <454752938@qq.com>
2021-01-08 17:13:17 +0800

    format

--- src/com/leetcode/ComputeArea.java
+++ src/com/leetcode/ComputeArea.java
@@ -7,9 +7,10 @@ package com.leetcode;
  */
 public class ComputeArea {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int x = Math.min(C, G) - Math.max(A, E);//重复的线段
-        int y = Math.min(D, H) - Math.max(B, F);
-        return (C - A) * (D - B) + (G - E) * (H - F) - x * y;
+        long x = Math.max(0, (long) Math.min(C, G) - Math.max(A, E));//重复的线段
+        long y = Math.max(0, (long) Math.min(D, H) - Math.max(B, F));
+        long area = (long) (C - A) * (D - B) + (G - E) * (H - F) - x * y;
+        return (int) area;
     }
 
     public static void main(String[] args) {
