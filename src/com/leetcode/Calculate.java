e9996dddc355e0c63a85a064f501e9edf3b07a89
chenqi <454752938@qq.com>
2021-06-22 19:21:26 +0800

    calculate

---
+++ src/com/leetcode/Calculate.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Description : 227.基本计算器II
+ * @Author : chen qi
+ * @Date: 2021-06-22 17:03
+ */
+public class Calculate {
+
+    public static int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate("32+2*2+2*2"));
+    }
+}
